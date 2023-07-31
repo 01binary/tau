51129337618a3706a466a9b2a966c837d979bf1f
Valeriy Novytskyy <valeriy.novytskyy@outlook.com>
2023-07-30 20:58:15 -0700

    Initial targeting executable source

---
+++ src/tau.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <opencv4/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+// ssh tau@10.0.0.47
+
+int main(int argc, const char** argv)
+{
+  VideoCapture cap(0);
+  Mat current_frame;
+
+  return 0;
+}
