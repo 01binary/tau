424a589a408a1ea86847378d6774fd3e4c3d25b5
01binary <valeriy.novytskyy@outlook.com>
2023-08-02 08:24:25 -0700

    Build configuration

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# tau
-Tau Cannon Replica from Half Life
+# Tau Cannon
+
+Tau Cannon Replica from Half Life: https://www.01binary.us/projects/tau-cannon
+
+# Building
+
+The on-board targeting computer uses OpenCV to perform pose detection.
+
+[Building OpenCV on Raspbery Pi 4](https://learnopencv.com/build-and-install-opencv-4-for-raspberry-pi/)
+
+```
+cd src
+cmake .
+make
+```
+
+This will output the executable called `tau` which can be set to run at startup.
--- src/tau.cpp
+++ src/tau.cpp
@@ -1,15 +1,58 @@
-#include <iostream>
-#include <opencv4/opencv.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
+using namespace cv::ml;
 
-// ssh tau@10.0.0.47
-
-int main(int argc, const char** argv)
+int main(void)
 {
   VideoCapture cap(0);
   Mat current_frame;
+  vector<Point> track;
+
+  HOGDescriptor hog;
+  hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+
+  while (true)
+  {
+    cap >> current_frame;
+
+    if (current_frame.empty())
+    {
+      cerr << "Video stream ended or bad frame." << endl;
+      return 1;
+    }
+
+    Mat img = current_frame.clone();
+    resize(img, img, Size(img.cols * 2, img.rows * 2));
+
+    vector<Rect> found;
+    vector<double> weights;
+
+    hog.detectMultiScale(img, found, weights);
+
+    // Draw detections
+    for (size_t i = 0; i < found.size(); i++)
+    {
+      Rect r = found[i];
+      rectangle(img, found[i], cv::Scalar(0, 0, 255), 3);
+
+      track.push_back(Point(
+        found[i].x + found[i].width / 2,
+        found[i].y + found[i].height / 2
+      ));
+    }
+
+    // Plot the track
+    for (size_t i = 1; i < track.size(); i++)
+    {
+      line(img, track[i - 1], track[i], Scalar(255, 255, 0), 2);
+    }
+
+    // Present
+    imshow("Targets", img);
+    waitKey(1);
+  }
 
   return 0;
 }
